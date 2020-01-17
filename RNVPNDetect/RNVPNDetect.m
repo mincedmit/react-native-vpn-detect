754d7e03c04df52be7b2b4f0a7bf17bcccf8420a
Sebastian Odell <sebastian@sanctuary.computer>
2020-01-17 17:53:53 -0500

    adds timer method (#2)

--- RNVPNDetect/RNVPNDetect.m
+++ RNVPNDetect/RNVPNDetect.m
@@ -2,7 +2,9 @@
 
 @implementation RNVPNDetect
 {
-  bool hasListeners;
+  BOOL hasListeners;
+  BOOL isVpnConnected;
+  NSTimer *timer;
 }
 
 -(void)startObserving {
@@ -18,26 +20,70 @@
   return @[@"RNVPNDetect.vpnStateDidChange"];
 }
 
-RCT_EXPORT_MODULE();
+- (void)sendIsVpnConnected:(NSTimer *)timer {
+    BOOL nextIsVpnConnected = [self isVpnConnected];
+    
+    if ((isVpnConnected && nextIsVpnConnected) || (!isVpnConnected && !nextIsVpnConnected)) {
+        return;
+    }
+    
+    isVpnConnected = nextIsVpnConnected;
+    if (hasListeners) {
+        [self sendEventWithName:@"RNVPNDetect.vpnStateDidChange" body:@(isVpnConnected)];
+    }
+}
 
-RCT_EXPORT_METHOD(isVPNConnected)
-{
+- (BOOL)isVpnConnected {
     CFDictionaryRef cfDict = CFNetworkCopySystemProxySettings();
     NSDictionary *nsDict = (__bridge NSDictionary*)cfDict;
     NSDictionary *keys = [nsDict valueForKey:@"__SCOPED__"];
-    __block BOOL isVpnConnected = nil;
-
+    BOOL isConnected = NO;
+    
     for (id key in keys) {
-        if ([@"tap" isEqual: key] || [@"tun" isEqual: key] || [@"ppp" isEqual: key] || [@"ipsec" isEqual: key] || [@"ipsec0" isEqual: key] || [key containsString: @"utun"] ) {
-            isVpnConnected = YES;
+        if ([@"tap" isEqual: key] || [@"tun" isEqual: key] || [@"ppp" isEqual: key] || [@"ipsec" isEqual: key] || [@"ipsec0" isEqual: key] || [key containsString: @"utun"]) {
+            isConnected = YES;
         } else {
-          isVpnConnected = NO;
+            isConnected = NO;
         }
     }
+    
+    return isConnected;
+}
 
+RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(startTimer:(NSTimeInterval)timerInterval)
+{
+    NSLog(@"%@", [NSDecimalNumber numberWithDouble:timerInterval]);
+    
+    NSTimeInterval interval;
+    if (timerInterval) {
+        interval = timerInterval;
+    } else {
+        interval = 3000;
+    }
+    
+    timer = [NSTimer timerWithTimeInterval:interval target:self selector:@selector(sendIsVpnConnected:) userInfo:nil repeats:YES];
+    [[NSRunLoop mainRunLoop] addTimer:timer forMode:NSRunLoopCommonModes];
+}
+
+RCT_EXPORT_METHOD(stopTimer)
+{
+    if (timer) {
+        [timer invalidate];
+    }
+    timer = nil;
+}
+
+
+RCT_EXPORT_METHOD(checkIsVpnConnected)
+{
+    isVpnConnected = [self isVpnConnected];
+    
     if (hasListeners) {
         [self sendEventWithName:@"RNVPNDetect.vpnStateDidChange" body:@(isVpnConnected)];
     }
 }
 @end
+
 
